39ea94310d3d25d933c1dad3af3f0afb29e3c921
Samuel Kolb <samuel.kolb@me.com>
2019-04-09 16:41:16 +0200

    Added tacle dependency

--- setup.py
+++ setup.py
@@ -9,11 +9,7 @@ setup(
     author_email="",
     license="",
     packages=["synthlog"],
-<<<<<<< HEAD
-    install_requires=["problog", "openpyxl"],
+    install_requires=["problog", "openpyxl", "problog"],
     entry_points={"console_scripts": ["synthlog=synthlog.__main__:main"]},
-=======
-    install_requires=["problog", "openpyxl", "tacle"],
->>>>>>> 8cd94af358f53e6db20635e70b4cdba36d010949
     zip_safe=False,
 )
