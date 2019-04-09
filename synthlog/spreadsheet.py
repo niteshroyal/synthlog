ad7ffce883eca4c08fdf16cef59f0052a0adcd07
clemgaut <clemgaut@gmail.com>
2019-04-09 15:35:42 +0200

    Adds documentation for load_spreadsheet function

--- synthlog/spreadsheet.py
+++ synthlog/spreadsheet.py
@@ -11,6 +11,20 @@ import openpyxl as xls
 
 @problog_export_nondet('+str', '-term')
 def load_spreadsheet(filename):
+    """
+    Load a excel spreadsheet into Synthlog.
+    Output is a list of cell terms, containing the values of the first excel spreadsheet.
+    For example, if on the second row and third column, there is a cell having value 6, the term cell(1, 2, 3, 6). will be created.
+    Usage:
+    .. code-block:: prolog
+        magic:X :- load_spreadsheet("magic.xlsx", X).
+
+    where X grounds to all non-empty cells in the spreadsheet.
+    :param filename: Filename of the xlsx file to load. If relative, the base directory is ...
+    :type filename: string
+    :return A list of cell terms. A cell term is cell(<sheet_id>, <row_id>, <column_id>, <value>)
+    :rtype list of Term
+    """
     # Resolve the filename with respect to the main Prolog file location.
     workbook = problog_export.database.resolve_filename(filename)
     if not os.path.isfile(workbook):
