34fa2e2db884bd4dae31ec8108911f7b267b47da
Arcchit Jain <arcchitjain@users.noreply.github.com>
2019-04-09 15:38:58 +0200

    ProbFOIL can use this file to learn rules directly

--- data/magic_ice_cream.pl
+++ data/magic_ice_cream.pl
@@ -1,10 +1,11 @@
-base(flavour(row_id, flavour_constant).
-base(country(row_id, country_constant).
-base(june(row_id, june_constant).
-base(july(row_id, july_constant).
-base(august(row_id, august_constant).
-base(total(row_id, total_constant).
-base(prodtime(row_id, prodtime_constant).
+% Typing of predicates
+base(flavour(row_id, flavour_constant)).
+base(country(row_id, country_constant)).
+base(june(row_id, june_constant)).
+base(july(row_id, july_constant)).
+base(august(row_id, august_constant)).
+base(total(row_id, total_constant)).
+base(prodtime(row_id, prodtime_constant)).
 
 base(flavour_vanilla(row_id)).
 base(flavour_banana(row_id)).
@@ -14,9 +15,9 @@ base(country_be(row_id)).
 base(country_de(row_id)).
 base(country_fr(row_id)).
 base(country_nl(row_id)).
-base(profit_yes(row_id)).
-base(profit_no(row_id)).
+base(profit(row_id)).
 
+% Declarative Bias
 mode(flavour(+,+)).
 mode(flavour(-,+)).
 mode(flavour(+,-)).
@@ -47,46 +48,48 @@ mode(country_be(+)).
 mode(country_de(+)).
 mode(country_fr(+)).
 mode(country_nl(+)).
-mode(profit_yes(+)).
-mode(profit_no(+)).
 
-learn(profit/2).
+% Target Predicate
+learn(profit/1).
 
+% Target Examples
+profit(r_1).
+profit(r_2).
+profit(r_3).
+0::profit(r_4).
+0::profit(r_5).
+
+% Background Facts
 flavour(r_1,vanilla).
 country(r_1,be).
 june(r_1,610).
 july(r_1,190).
 august(r_1,670).
 total(r_1,1470).
-profit(r_1,yes).
 flavour(r_2,banana).
 country(r_2,be).
 june(r_2,170).
 july(r_2,690).
 august(r_2,520).
 total(r_2,1380).
-profit(r_2,yes).
 flavour(r_3,chocolate).
 country(r_3,be).
 june(r_3,560).
 july(r_3,320).
 august(r_3,140).
 total(r_3,1020).
-profit(r_3,yes).
 flavour(r_4,banana).
 country(r_4,de).
 june(r_4,610).
 july(r_4,640).
 august(r_4,320).
 total(r_4,1570).
-profit(r_4,no).
 flavour(r_5,speculaas).
 country(r_5,be).
 june(r_5,300).
 july(r_5,270).
 august(r_5,290).
 total(r_5,860).
-profit(r_5,no).
 flavour(r_6,chocolate).
 country(r_6,fr).
 june(r_6,430).
@@ -109,16 +112,12 @@ flavour(r_12, vanilla).
 prodtime(r_12, 40).
 flavour_vanilla(r_1).
 country_be(r_1).
-profit_yes(r_1).
 flavour_banana(r_2).
 country_be(r_2).
-profit_yes(r_2).
 flavour_chocolate(r_3).
 country_be(r_3).
-profit_yes(r_3).
 flavour_banana(r_4).
 country_de(r_4).
-profit_no(r_4).
 flavour_speculaas(r_5).
 country_be(r_5).
 flavour_chocolate(r_9).
