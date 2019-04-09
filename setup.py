a7e8bdad162be49d675a99eeca03c49328fa198a
Gust Verbruggen <gust@Gusts-MacBook-Pro.local>
2019-04-09 14:00:16 +0200

    package structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="synhlog",
+    version="0.0",
+    description="",
+    url="",
+    author="",
+    author_email="m",
+    license="",
+    packages=["synthlog"],
+    zip_safe=False,
+)
+
