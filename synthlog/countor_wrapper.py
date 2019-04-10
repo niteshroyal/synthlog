1b2478fb58b0a4474807cddbe95dbc608d629732
mohitkr <mohitkrg@gmail.com>
2019-04-10 17:46:17 +0200

    countor code

--- synthlog/countor_wrapper.py
+++ synthlog/countor_wrapper.py
@@ -1,22 +1,33 @@
 # from extended_db import save
 import dill as pickle
-import sys
-
-from problog.extern import problog_export, problog_export_raw
-from problog.logic import Constant, Object, Term, Var, unquote, term2list
-from problog.errors import UserError
-
+from problog.extern import problog_export, problog_export_raw, problog_export_nondet
+from problog.logic import Object, Term
 from countor.count_or import learnConstraints
+from countor.count_or import cleanData
+import numpy as np
+import synthlog.spreadsheet as spreadsheet
 
+
+@problog_export_nondet("+term",  "-term")
+def learn_count_or_constraints(scope,  **kwargs):
+    table_cell_term_list=spreadsheet.get_terms_from_scope(scope,"table_cell",kwargs)
+    matrix = spreadsheet.table_cells_to_matrix(table_cell_term_list)
+    dataTensor,variables=cleanData(matrix)
+    constraints = learnConstraints(dataTensor,dataTensor.shape)
+    print(constraints)
+    return constraints
 
 @problog_export("+term", "-term")
-def learn_count_or_constraints(matrix, *args, **kwargs):
-    print(matrix, type(matrix), dir(matrix))
-    print(matrix.functor)
-    print(matrix.functor.matrix)
-    quit()
-    m = matrix.functor.matrix
-    constraints = co.learnConstraints(m, m.shape)
+def get_count_or_constraints(matrix, *args, **kwargs):
+#    print(matrix, type(matrix), dir(matrix))
+#    print(matrix.functor)
+    data=np.asarray(matrix.functor.matrix)
+    dataTensor,variables=cleanData(data)
+#    print(dataTensor,variables)
+#    quit()
+#    m = matrix.functor.matrix
+    constraints = learnConstraints(dataTensor,dataTensor.shape)
+    print(constraints)
     return Object(constraints)
 
 
