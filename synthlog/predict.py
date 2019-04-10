00766acae4f57fc803780e94d064b02d9f847815
clemgaut <clemgaut@gmail.com>
2019-04-10 18:07:46 +0200

    Adds documentation for predict and decision_tree functions (not in stone, might change).

--- synthlog/predict.py
+++ synthlog/predict.py
@@ -45,6 +45,18 @@ def comparison(column1, column2):
 
 @problog_export_nondet("+term", "+list", "+list", "-term")
 def decision_tree(scope, source_columns, target_columns, **kwargs):
+    """
+    Learn a decision tree predictor on scope. It uses source_columns to predict target_columns
+    :param scope: A scope, containing table_cell predicates describing a table content.
+    :param source_columns: A list of columns, where column is: column(<table_name>, <col_number>). <table_name> is a table name present in table_cell. These columns will be used as input columns for the predictor.
+    :param target_columns: A list of columns, where column is: column(<table_name>, <col_number>). <table_name> is a table name present in table_cell. These columns will be used as columns to predict for the predictor.
+    :param kwargs:
+    :return: A list of Terms.
+    predictor(<predictor>) is created, with <predictor> the scikit-learn predictor object.
+    decision_tree(<predictor> is created, with <predictor> the scikit-learn predictor object.
+    target(<predictor>, <column>) are created for each target column. <predictor> is the scikit-learn predictor object and <column> is column(<table_name>, <col_number>)
+    source(<predictor>, <column>) are created for each source column. <predictor> is the scikit-learn predictor object and <column> is column(<table_name>, <col_number>)
+    """
     engine = kwargs["engine"]
     database = kwargs["database"]
     table_cell_term_list = [
@@ -76,6 +88,20 @@ def decision_tree(scope, source_columns, target_columns, **kwargs):
 
 @problog_export_nondet("+term", "+term", "+list", "-term")
 def predict(scope, predictor, source_columns, **kwargs):
+    """
+    Predict values using a predictor that was fitted on data. It uses source_columns of scope to predict the data
+    :param scope: A scope, containing table_cell predicates describing a table content.
+    :param predictor: A scikit-learn predictor, stored as a Problog Object (accessible through predictor(<predictor>) of the decision_tree funtion).
+    :param source_columns: A list of columns, where column is: column(<table_name>, <col_number>). <table_name> is a table name present in table_cell. These columns will be used as input columns for the predictor.
+    :param kwargs:
+    :return: Predictions from predictor using source_columns of scope, as well as predictions metadata.
+    prediction(<scope>, <predictor>, <source_columns>) is created. <scope> is the scope parameter, as a Problog object, <predictor> is the predictor parameter, as a Problog object and <source_columns> are the source_columns parameter as a Problog object.
+        This whole prediction/3 is used as a key for the prediction object. In the future, it might be better to use a unique identifier or something else!
+    cell_pred(<row_id>, <col_id>, <value>, <prediction_term>) are created for each prediction. <row_id> and <col_id> are indexed from (0,0), and NOT from the table_cell row and column ids.
+        The <col_id> corresponds to the index of the target column of predictor. <value> is the predicted value. <prediction_term> is whole prediction(<scope>, <predictor>, <source_columns>) defined above.
+    predictor(<prediction_term>, <predictor>) is created. <prediction_term> is whole prediction(<scope>, <predictor>, <source_columns>) defined above, <predictor> is the predictor parameter, as a Problog object
+    source(<prediction_term>, <source_column>) are created for each source_column. <prediction_term> is whole prediction(<scope>, <predictor>, <source_columns>) defined above, <source_column> is column(<table_name>, <col_number>)
+    """
     prediction_term_3 = Term(
         "prediction", Object(scope), Object(predictor), Object(source_columns)
     )
