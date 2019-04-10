dc79f5cf3c0364967949e1029106a77737a72139
eliavw <elia.vw@gmail.com>
2019-04-10 18:02:13 +0200

    small multi tree example, not done yet

--- examples/multi_dt_example.pl
+++ examples/multi_dt_example.pl
@@ -8,6 +8,23 @@ magic_tables:X :- detect_tables(magic_cells, X).
 
 % Train a classifier
 magic_models:X :-decision_tree(magic_tables,
-                                [column('T1', 2), column('T1', 3)],
-                                [column('T1', 4)],
-                                X).
+                               [column('T1', 2), column('T1', 3)],
+                               [column('T1', 4)],
+                               X).
+
+magic_models:X :-decision_tree(magic_tables,
+                               [column('T1', 3), column('T1', 5)],
+                               [column('T1', 4)],
+                               X).
+
+% Get all predictors in your scope
+my_predictors(Z):-magic_models:predictor(Z).
+query(my_predictors(_)).
+
+% All sources of these predictors
+src_predictors(Z):-my_predictors(X), magic_models:source(X, Z).
+query(src_predictors(_)).
+
+% All targets of these predictors
+tgt_predictors(Z):-my_predictors(X), magic_models:target(X, Z).
+query(tgt_predictors(_)).
