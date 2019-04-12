6e79d6eaf42e8333116e514bc8ebc6cc8dc49d0f
Arcchit Jain <arcchit.jain.2015@iitkalumni.org>
2019-04-12 17:40:41 +0200

    Subquerying the probfoil rules

--- examples/probfoil_example.pl
+++ examples/probfoil_example.pl
@@ -20,8 +20,7 @@ magic_tables:table_atoms(Table_name, Cell_header, Cell_row, Cell_type, Cell_valu
     magic_tables:table_header(Table_name, Cell_column, Cell_header),
     magic_tables:table_cell(Table_name, Cell_row, Cell_column, Cell_value),
     magic_tables:table_cell_type(Table_name, Cell_row, Cell_column, Cell_type).
-%query(magic_tables:table_atoms(_,_,_,_,_)).
-
+% query(magic_tables:table_atoms(_,_,_,_,_)).
 
 % Convert the matrix of cells to atoms
 magic_atoms:Atom :-
@@ -32,12 +31,53 @@ magic_atoms:Atom :-
 % Learn probfoil rules for all atoms in the scope 'magic_atoms' with 'profit' as our target predicate
 probfoil_rules:Rule :- probfoil(magic_atoms, 'profit', Rule).
 probfoil_rules:Rule :- probfoil_loop(magic_atoms, 'country', Rule).
-query(probfoil_rules:blackbox_rule(_,_,_)).
+% query(probfoil_rules:blackbox_rule(_,_,_)).
 
 % Unify the facts with the rules
 rules_and_facts:X :- magic_atoms:X; probfoil_rules:X.
 % query(rules_and_facts:_).
 
 %:- parse_clause_from_term(Rule),rules_and_facts:blackbox_rule(Target, Rule_number, Rule).
-% rules:Rule :- probfoil_rules:blackbox_rule(Target, Rule_num, Rule), parse_clause(Rule), writenl('Parsed rule: ', Rule).
+rules:Rule :- probfoil_rules:blackbox_rule(Target, Rule_num, Rule), parse_clause(Rule). %, writenl('Parsed rule: ', Rule).
 % query(rules:_).
+
+%% Predict cells from rules
+% Input:
+%   Scope of independent cells : All cells - cells to be predicted
+%   Scope of rules
+%   Target_header
+%   Table_name
+%   Cell_row
+%   Cell_column
+% Output:
+%   Predicted Cell Value
+
+atoms_without_profit:X :-
+    magic_atoms:X,
+    \+ X =.. [profit|_],
+    \+ X =.. [profit_yes|_],
+    \+ X =.. [profit_no|_].
+
+%profit:X :- magic_atoms:X, X =.. [profit|_].
+%profit:profit(X) :- profit:profit(X, 'yes').
+%0::profit:profit(X) :- profit:profit(X, 'no').
+%atoms_with_profit:X :- profit:X ; atoms_without_profit:X.
+%query(profit:_).
+
+atoms_with_rules:X :- atoms_without_profit:X; rules:X.
+
+
+0::atoms_with_rules:type_speculaas(1).
+0::atoms_with_rules:country_de(1).
+0::atoms_with_rules:country_nl(1).
+
+atoms_with_rules:profit(A) :-
+    atoms_with_rules:type_speculaas(A),
+    \+atoms_with_rules:country_de(A),
+    \+atoms_with_rules:country_nl(A).
+
+P::profit(X) :- subquery(atoms_with_rules:profit(X), P, []), writenl('Probability of ', X, ':', P).
+%query(atoms_with_rules:_).
+%query(atoms_with_rules:profit(_)).
+query(profit(_)).
+
