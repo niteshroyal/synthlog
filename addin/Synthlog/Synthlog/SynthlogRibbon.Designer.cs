d2e4f0ca9d43c84680729016219796267813ba82
Yann Dauxais <yann.dauxais@cs.kuleuven.be>
2019-05-08 17:38:31 +0200

    init addin

---
+++ addin/Synthlog/Synthlog/SynthlogRibbon.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Synthlog
+{
+    partial class SynthlogRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public SynthlogRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.synthlog = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.toggleButton1 = this.Factory.CreateRibbonToggleButton();
+            this.synthlog.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // synthlog
+            // 
+            this.synthlog.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.synthlog.Groups.Add(this.group1);
+            this.synthlog.Label = "Synthlog";
+            this.synthlog.Name = "synthlog";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.toggleButton1);
+            this.group1.Name = "group1";
+            // 
+            // toggleButton1
+            // 
+            this.toggleButton1.Label = "Show panel";
+            this.toggleButton1.Name = "toggleButton1";
+            this.toggleButton1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.toggleButton1_Click);
+            // 
+            // SynthlogRibbon
+            // 
+            this.Name = "SynthlogRibbon";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.synthlog);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.synthlog.ResumeLayout(false);
+            this.synthlog.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab synthlog;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonToggleButton toggleButton1;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal SynthlogRibbon Ribbon1
+        {
+            get { return this.GetRibbon<SynthlogRibbon>(); }
+        }
+    }
+}
