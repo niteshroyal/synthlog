ad2e2768a66c56dc061257cfc1af6b4e2789d651
clemgaut <clemgaut@gmail.com>
2019-05-08 15:54:24 +0200

    Updates example to new probabilistic prediction.

--- examples/predictor_proba_example.pl
+++ examples/predictor_proba_example.pl
@@ -21,23 +21,14 @@ magic_models:X :-random_forest(magic_tables,
 % Do a probabilistic prediction
 Prob::predict(S, Pred, C, X) :- predict(S, Pred, C, X, Prob).
 
-
 magic_predict:X :- magic_models:predictor(Y), predict(magic_tables, Y, [column('T1', 2), column('T1', 3)], X), magic_models:source(Y, column('T1', 2)), magic_models:source(Y, column('T1', 3)).
 magic_predict:X :- magic_models:predictor(Y), predict(magic_tables, Y, [column('T1', 2), column('T1', 5)], X), magic_models:source(Y, column('T1', 2)), magic_models:source(Y, column('T1', 5)).
 
-% We get the confidence of each classifier
-confidence1 :- magic_predict:source(C1, column('T1', 3)), magic_predict:cell_pred(X,Y,V,C1).
-confidence2 :- magic_predict:source(C2, column('T1', 5)), magic_predict:cell_pred(X,Y,V,C2).
-
-% These 2 predictors should be in an annotated disjunction
-% This part seems to work but I am not 100 pct sure of it....
-\+ confidence2 :- confidence1.
+% This emulates an annotated disjunction
+weight(X) :- magic_predict:source(C1, column('T1', 3)), magic_predict:confidence(C1, Conf1), magic_predict:source(C2, column('T1', 5)), magic_predict:confidence(C2, Conf2), select_weighted(1, [Conf1, Conf2], [C1, C2], X, _).
 
 % We combine the prediction using the confidences
-magic_predict:final_pred(X,Y,V):- confidence1, magic_predict:source(C1, column('T1', 3)), magic_predict:cell_pred(X,Y,V,C1).
-magic_predict:final_pred(X,Y,V):- confidence2, magic_predict:source(C2, column('T1', 5)), magic_predict:cell_pred(X,Y,V,C2).
-
-
+magic_predict:final_pred(X,Y,V):- weight(C1), magic_predict:source(C1, column('T1', 3)), magic_predict:cell_pred(X,Y,V,C1).
+magic_predict:final_pred(X,Y,V):- weight(C2), magic_predict:source(C2, column('T1', 5)), magic_predict:cell_pred(X,Y,V,C2).
 
 query(magic_predict:final_pred(_,_,_)).
-%query(magic_predict:cell_pred(_,_,_, _)).
