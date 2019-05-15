595373b2bac0cdca1423b71f8e599b6f1fe77124
Anonymous <>
2019-05-15 14:57:41 +0200

    debug head copy

--- synthlog/clause.py
+++ synthlog/clause.py
@@ -4,17 +4,18 @@ from problog import get_evaluatable
 from problog.extern import problog_export, problog_export_nondet, problog_export_raw
 
 from problog.logic import Clause, Constant
+from copy import deepcopy
 
 
 @problog_export_nondet("+term", "-term", "-term", "-term")
 def unify_clause(clause):
     if isinstance(clause, Clause):
-        head = clause.head
+        head = deepcopy(clause.head)
         proba = head.probability
         if proba is None:
             proba = 1
+        head.probability = None
         if not isinstance(proba, Constant):
             proba = Constant(proba)
-        head.probability = None
-        return [(head, proba, clause.body)]
+        return [(head, Constant(proba), clause.body)]
     return []
