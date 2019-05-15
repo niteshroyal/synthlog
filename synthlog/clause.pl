501c23fc2325b3e166c0320f8406536db0e8ba7d
Anonymous <>
2019-05-15 15:37:41 +0200

    debug clauses

--- synthlog/clause.pl
+++ synthlog/clause.pl
@@ -15,14 +15,14 @@ evaluate(Scope,(X > Y)) :- ground(X), ground(Y), X > Y.
 evaluate(Scope,(X >= Y)) :- ground(X), ground(Y), X >= Y.
 %% X == Y
 evaluate(Scope, (X == Y)) :- ground(X), X == Y.
-evaluate(Scope, (X == Y)) :- evaluate(Scope, (Y == X)).
+evaluate(Scope, (X == Y)) :- ground(Y), X == Y.
 %% X \== Y
 evaluate(Scope, (X \== Y)) :- ground(X), ground(Y), X \== Y.
 %% X is Y
 evaluate(Scope,(X is Y)) :- ground(Y), X is Y.
 %% X, Y
 evaluate(Scope,(X,Y)) :- evaluate(Scope,X), evaluate(Scope,Y).
-evaluate(Scope,(X,Y)) :- evaluate(Scope,(Y,X)).
+evaluate(Scope,(X,Y)) :- evaluate(Scope,Y), evaluate(Scope,X).
 %% X; Y
 evaluate(Scope,(X;Y)) :- evaluate(Scope,X); evaluate(Scope,Y).
-evaluate(Scope,(X;Y)) :- evaluate(Scope,(Y;X)).
+evaluate(Scope,(X;Y)) :- evaluate(Scope,Y); evaluate(Scope,X).
