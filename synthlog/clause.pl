509bf575cdc612e821b636cb4aa2fdb45e9b4742
Anonymous <>
2019-05-15 14:16:46 +0200

    add clause rules

--- synthlog/clause.pl
+++ synthlog/clause.pl
@@ -5,9 +5,24 @@ Scope:X :- Scope:Z, unify_clause(Z,X,Y), evaluate(Scope,Y).
 
 % Evaluation rules
 evaluate(Scope,Y) :- Scope:Y.
+%% X < Y
 evaluate(Scope,(X < Y)) :- ground(X), ground(Y), X < Y.
+%% X <= Y
+evaluate(Scope,(X <= Y)) :- ground(X), ground(Y), X <= Y.
+%% X > Y
+evaluate(Scope,(X > Y)) :- ground(X), ground(Y), X > Y.
+%% X >= Y
+evaluate(Scope,(X >= Y)) :- ground(X), ground(Y), X >= Y.
+%% X == Y
+evaluate(Scope, (X == Y)) :- ground(X), X == Y.
+evaluate(Scope, (X == Y)) :- evaluate(Scope, (Y == X)).
+%% X \== Y
+evaluate(Scope, (X \== Y)) :- ground(X), ground(Y), X \== Y.
+%% X is Y
 evaluate(Scope,(X is Y)) :- ground(Y), X is Y.
+%% X, Y
 evaluate(Scope,(X,Y)) :- evaluate(Scope,X), evaluate(Scope,Y).
 evaluate(Scope,(X,Y)) :- evaluate(Scope,(Y,X)).
+%% X; Y
 evaluate(Scope,(X;Y)) :- evaluate(Scope,X); evaluate(Scope,Y).
 evaluate(Scope,(X;Y)) :- evaluate(Scope,(Y;X)).
