daa6962754e921207b29e3a75e43330e7f290432
clemgaut <clemgaut@gmail.com>
2019-05-16 17:10:50 +0200

    Updates ProbFoil to make clauses work in Scopes (negative term not supported though...)

--- synthlog/spreadsheet.py
+++ synthlog/spreadsheet.py
@@ -212,7 +212,9 @@ def translate_constraint(constraint: Constraint):
         )
 
 
-@problog_export_nondet("+term", "+term", "+term", "+term", "+term", "+term", "-term")
+@problog_export_nondet(
+    "+term", "+term", "+term", "+term", "+term", "+term", "-term", "-float"
+)
 def cell_to_atoms(
     table_name,
     header,
@@ -234,15 +236,23 @@ def cell_to_atoms(
 
     # result = [Term(header, Constant(row_id), Constant(cell_value))]
     result = []
+
+    result.append((Term("row", Constant(row_id)), 1.0))
+
     if column_type == "string":
         for unique_value in column_unique_values1:
-            if unquote(unique_value) == cell_value:
-                # result.append(Term(header + "_" + cell_value, Constant(row_id)))
-                r = 0
+            if unquote(unique_value).lower() == cell_value:
+                result.append((Term(header + "_" + cell_value, Constant(row_id)), 1.0))
             else:
                 # WIth Probability 0
                 result.append(
-                    Term(header + "_" + cell_value, Constant(row_id), p=Constant(0.0))
+                    (
+                        Term(
+                            header + "_" + unquote(unique_value).lower(),
+                            Constant(row_id),
+                        ),
+                        0.0,
+                    )
                 )
     return result
 
