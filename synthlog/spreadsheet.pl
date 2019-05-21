2db52fee5c45259b0545c2407b04db0605502845
Anonymous <>
2019-05-21 17:47:01 +0200

    add subquery on cells

--- synthlog/spreadsheet.pl
+++ synthlog/spreadsheet.pl
@@ -3,4 +3,4 @@
 wrong :- S:table_cell(T, X, Y, V2), S:table_cell(T, X, Y, V1), V1 \== V2. % no more than 1 prediction per table for each x,y
 wrong :- possible(S:table_cell(T, X, Y, _)), \+ S:table_cell(T, X, Y, V). % at least one pred per cell
 
-detect_tables(Scope, Term) :- findall((P::cell(X,Y,Z)), Scope:cell(X,Y,Z), List), detect_cell_tables(List, Term).
+detect_tables(Scope, Term) :- findall((P::cell(X,Y,Z)), subquery(Scope:cell(X,Y,Z), P), List), detect_cell_tables(List, Term).
