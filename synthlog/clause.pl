73626bd0dfe162bdbb475acef0b4af2373f23b94
clemgaut <clemgaut@gmail.com>
2019-05-22 15:57:12 +0200

    reverts to old closes, without negation working

--- synthlog/clause.pl
+++ synthlog/clause.pl
@@ -1,15 +1,17 @@
 :- use_module('clause.py').
 
 % Clause parsing
-:- Scope:Z, apply_clause(Scope,Z).
-% P::Scope:X :- Scope:Z, Z \== X, unify_clause(Z,X,P,Y), evaluate(Scope,Y).
+%:- contains_clauses(Scope), Scope:Z, apply_clause(Scope,Z).
+P::Scope:X :- contains_clauses(Scope), Scope:Z, Z \== X, unify_clause(Z,X,P,Y), evaluate(Scope,Y).
+
+contains_clauses(dummyscopeble).
 
 % Evaluation rules
 evaluate(Scope,Y) :- Scope:Y.
 evaluate(Scope,var(Y)) :- Scope:Y.
 %% \+ Y
-evaluate(Scope, \+ Y) :- \+ evaluate(Scope, Y). % (evaluate(Scope,Y), store_existing_term(Y)); is_not_existing_term(Y).
-evaluate(Scope, not(Y)) :- evaluate(Scope, \+ Y).
+%evaluate(Scope, \+ Y) :- \+ evaluate(Scope, Y). % (evaluate(Scope,Y), store_existing_term(Y)); is_not_existing_term(Y).
+%evaluate(Scope, not(Y)) :- evaluate(Scope, \+ Y).
 %% X < Y
 evaluate(Scope,(X < Y)) :- ground(X), ground(Y), X < Y.
 %% X <= Y
--- synthlog/environment.pl
+++ synthlog/environment.pl
@@ -1,12 +1,12 @@
-:- use_module('spreadsheet.pl').
-:- use_module('transformers.pl').
 :- use_module(library(collect)).
 :- use_module(library(aggregate)).
 :- use_module(library(lists)).
 list(X,X).
+:- use_module('spreadsheet.pl').
+:- use_module('clause.pl').
 :- use_module('../synthlog/utils.py').
 :- use_module('predict.pl').
-:- use_module('clause.pl').
+:- use_module('transformers.pl').
 
 wrong :- S:wrong.
 
