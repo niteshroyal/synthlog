83174d0899ff146a0ab95f90adc825cadce7f7b2
Yann Dauxais <yann.dauxais@cs.kuleuven.be>
2019-06-20 11:27:52 +0200

    add synthlog calls

--- addin/SynthlogReactAddIn/Synthlog/src/server/index.js
+++ addin/SynthlogReactAddIn/Synthlog/src/server/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 // const pino = require('express-pino-logger')();
 const cors = require('cors');
 var structure = require('./librairies/structure');
+var { PythonShell } = require('python-shell');
 
 const app = express();
 app.use(cors());
@@ -16,11 +17,28 @@ app.get('/api/init', (req, res) => {
     res.send(JSON.stringify({ init: inited }));
 });
 
-app.get('/api/greeting', (req, res) => {
-  const name = req.query.name || 'World';
-  console.log(`Hello ${name}!`);
+app.get('/api/check_python', (req, res) => {
+    PythonShell.runString('x=1;', null, function(err, results){
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({ python: !err }));
+    });
+});
+
+app.get('/api/log', (req, res) => {
+  const type = req.query.type || 'Log';
+  console.log(`${type}: ${req.query.message}`);
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ greeting: `Hello ${name}` }));
+  res.send(JSON.stringify({ log: true }));
+});
+
+app.use(express.json());
+app.post('/api/run_synthlog', (req, res) => {
+    console.log("here!");
+    console.log(req.body);
+    if (req.body.script) {
+        structure.generateParameters(req.body);
+        structure.runScript(req.body.script, res);
+    }
 });
 
 // start the app
--- addin/SynthlogReactAddIn/Synthlog/src/server/librairies/structure.js
+++ addin/SynthlogReactAddIn/Synthlog/src/server/librairies/structure.js
@@ -3,9 +3,12 @@ const FileSystem = require('fs');
 const Request = require('request');
 const Os = require('os');
 const Path = require('path');
+const { PythonShell } = require('python-shell');
+const Process =require('process');
 
 var homedir = Path.resolve(Os.homedir(), ".SynthLogBackEnd"); 
 var inited = false;
+var problog = false;
 
 function createDir(path) {
     if (!FileSystem.existsSync(path))
@@ -25,9 +28,58 @@ function downloadProblog() {
                     Path.resolve(homedir, "problog-master"),
                     Path.resolve(homedir, "problog")
                 );
-                inited = true;
+                problog = true;
             }
         );
+}
+
+function generateCells(cells) {
+    var res = []
+
+    if(cells) {
+        var row_number = cells.values.length;
+        var column_number = cells.values[0].length;
+
+        for (var i = 0; i < row_number; i++) {
+            for (var j = 0; j < column_number; j++) {
+                res.push([
+                    i+cells.firstRow, 
+                    j+cells.firstColumn,
+                    cells.values[i][j]
+                ])
+            }
+        }
+    }
+
+    return res;
+}
+
+exports.generateParameters = function(parameters) {
+    const cells = generateCells(parameters.cells);
+    terms = generateTermStrings('cell', cells);
+    for (var key in parameters) {
+        if (!["cells", "script"].includes(key)) {
+            if (Array.isArray(parameters[key]))
+                terms += generateTermStrings(key, parameters[key]);
+            else
+                terms += generateTermStrings(key, [[parameters[key]]]);
+        }
+    }
+    FileSystem.writeFileSync(Path.resolve(homedir, 'parameters.pl'), terms);
+}
+
+function generateTermStrings(key, values) {
+    terms = "";
+    values.forEach(element => {
+        args = "";
+        element.forEach(arg => { 
+            if (args != "") args += ","; 
+            args += arg.toString();
+        });
+        if (args != "") args = "(" + args + ")";
+        terms += "excel:" + key + args + ". ";
+    });
+    return terms;
 }
 
 exports.init = function() {
@@ -36,6 +88,7 @@ exports.init = function() {
         var problog_path = Path.resolve(homedir, "problog");
         if (!FileSystem.existsSync(problog_path))
             downloadProblog();
+        else problog = true;
 
         var synthlog_path = Path.resolve(homedir, "synthlog");
         if (!FileSystem.existsSync(synthlog_path))
@@ -53,11 +106,42 @@ exports.init = function() {
     }
     catch(error) {
         console.error(error);
-        return false;
+        return inited = false;
     }
-    return true;
+
+    return inited = true;
 }
 
 function importSynthlog() {
+    const path = Path.resolve(homedir, "synthlog");
+    var zip = AdmZip(Path.resolve(__dirname, "..", "resources", "synthlog.zip"));
+    zip.extractAllToAsync(path, true);
+}
 
+exports.runScript = function(filename, res) {
+    const problog_path = Path.resolve(homedir, 'problog');
+    Process.chdir(Path.resolve(homedir, 'synthlog'));
+    const options = {
+        mode: 'text',
+        scriptPath: problog_path,
+        pythonOptions: ['-u'],
+        args: [
+            '--combine',
+            Path.resolve(homedir, 'synthlog', 'synthlog', 'environment.pl'),
+            Path.resolve(homedir, 'parameters.pl'),
+            Path.resolve(homedir, filename)
+        ]
+    };
+    PythonShell.run('problog-cli.py', options, function(err, results) {
+        if (err) {
+            console.error(err.message);
+            console.error(err.stack);
+            res.setHeader('Content-Type', 'application/json');
+            res.send({error: err});
+        }
+        else {
+            res.setHeader('Content-Type', 'application/json');
+            res.send({output: results});
+        }
+    });
 }
