14277168caa4586307845179730ecad4264ac971
Yann Dauxais <yann.dauxais@cs.kuleuven.be>
2019-07-15 15:52:55 +0200

    new init.pl and load_active.pl for new inductive_db

--- addin/SynthlogReactAddIn/Synthlog/src/server/resources/builtin/init.pl
+++ addin/SynthlogReactAddIn/Synthlog/src/server/resources/builtin/init.pl
@@ -1,2 +1,4 @@
-idb(IDB) :- excel(parameters):idb(X), load_prob_inductive_db(X, IDB). 
-query_save_term(Scope, excel(data):X, IDB) :- excel(parameters):scope(Scope), idb(IDB). 
+:- excel(parameters):idb(IDB), load_prob_inductive_db(IDB). 
+query_save_term(Scope, excel(data):X, IDB) :- 
+    excel(parameters):scope(Scope), 
+    excel(parameters):idb(IDB). 
--- addin/SynthlogReactAddIn/Synthlog/src/server/resources/builtin/load_active.pl
+++ addin/SynthlogReactAddIn/Synthlog/src/server/resources/builtin/load_active.pl
@@ -1,4 +1,4 @@
-idb(IDB) :- excel(parameters):idb(X), load_prob_inductive_db(X, IDB). 
+:- excel(parameters):idb(IDB), load_prob_inductive_db(IDB). 
 result:X :- idb(IDB), excel(parameters):scope(Scope), Scope:X. 
 theory:Scope :- excel(parameters):scope(Scope). 
 active:Scope :- theory:Scope. 
